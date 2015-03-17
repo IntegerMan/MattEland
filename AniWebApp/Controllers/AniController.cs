9b7955d0a14b9ba38d7c94e2c745b327447380e6
Matt Eland <Matt.Eland@Outlook.com>
2015-03-16 23:04:14 -0400

    Added web site. Why not?

---
+++ AniWebApp/Controllers/AniController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AniWebApp.Controllers
+{
+    public class AniController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
