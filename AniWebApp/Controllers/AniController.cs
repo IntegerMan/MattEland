8cd1c8d63615d533c3b910505cb4f9adfc9732d1
Matt Eland <Matt.Eland@Outlook.com>
2015-03-18 23:37:31 -0400

    Added a Frost page. Once database migration is done, I'll revise this to hold more data.

--- AniWebApp/Controllers/AniController.cs
+++ AniWebApp/Controllers/AniController.cs
@@ -1,18 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace AniWebApp.Controllers
 {
+    /// <summary>
+    /// A Controller related to all ANI-related items
+    /// </summary>
     public class AniController : Controller
     {
+        /// <summary>
+        /// Gets the main ANI view
+        /// </summary>
+        /// <returns>ActionResult.</returns>
+        [HttpGet]
+        [Route(@"Ani")]
         public ActionResult Index()
         {
             return View();
         }
 
+        /// <summary>
+        /// Gets data for the weather forecasts page.
+        /// </summary>
+        /// <returns>ActionResult.</returns>
+        [HttpGet]
+        [Route(@"Ani\Weather\Forecasts")]
         public ActionResult Forecasts()
         {
             var entities = new AniEntities();
@@ -21,6 +34,20 @@ namespace AniWebApp.Controllers
             const int ZipCode = 43035;
 
             var predictions = entities.ActiveWeatherPredictionsSelect(ZipCode).ToList();
+            return View(predictions);
+        }
+
+        /// <summary>
+        /// Gets data for the weather forecasts page.
+        /// </summary>
+        /// <returns>ActionResult.</returns>
+        [HttpGet]
+        [Route(@"Ani\Weather\Frost")]
+        public ActionResult Frost()
+        {
+            var entities = new AniEntities();
+
+            var predictions = entities.FrostPredictionDataViews.ToList();
             return View(predictions);
         }
 
