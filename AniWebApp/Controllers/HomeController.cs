786f7c0ebd39bbcb81836ad17e0f6bbbda8a8eb5
Matt Eland <Matt.Eland@Outlook.com>
2015-03-22 18:18:28 -0400

    Documented controller.

--- AniWebApp/Controllers/HomeController.cs
+++ AniWebApp/Controllers/HomeController.cs
@@ -3,16 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace AniWebApp.Controllers
 {
-    public class HomeController : Controller
+    /// <summary>
+    /// An MVC Controller controlling the application root.
+    /// </summary>
+    public class HomeController : CustomController
     {
+        /// <summary>
+        /// Serves up the home page
+        /// </summary>
+        /// <returns>The home page view</returns>
         public ActionResult Index()
         {
             return View();
         }
 
+        /// <summary>
+        /// Serves up the about me page
+        /// </summary>
+        /// <returns>The about me view.</returns>
         public ActionResult AboutMe()
         {
             return View();
