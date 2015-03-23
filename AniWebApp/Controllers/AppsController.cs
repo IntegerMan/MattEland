71b2ec77c9b36f2639068108c7b013d10821be4c
Matt Eland <Matt.Eland@Outlook.com>
2015-03-22 23:14:37 -0400

    Made a page to display apps. I'll likely extract a layout from this.

--- AniWebApp/Controllers/AppsController.cs
+++ AniWebApp/Controllers/AppsController.cs
@@ -6,9 +6,21 @@ using System.Web.Mvc;
 
 namespace AniWebApp.Controllers
 {
-    public class AppsController : Controller
+    /// <summary>
+    /// MVC Controller for Windows Phone application info pages
+    /// </summary>
+    public class AppsController : CustomController
     {
+        [HttpGet]
+        [Route("Apps")]
         public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Route(@"Apps/SeekingGod")]
+        public ActionResult SeekingGod()
         {
             return View();
         }
