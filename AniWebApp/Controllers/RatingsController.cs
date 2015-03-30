e43d71ed4a870fb9d4ba4bcbdaa98c6a6dedb449
Matt Eland <Matt.Eland@Outlook.com>
2015-03-30 01:22:33 -0400

    Expanded site to encompass some of Sprint 9's activities.

---
+++ AniWebApp/Controllers/RatingsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AniWebApp.Controllers
+{
+    /// <summary>
+    /// An MVC Controller governing ratings-related activities
+    /// </summary>
+    public class RatingsController : CustomController
+    {
+        /// <summary>
+        /// Goes to the main index page for the ratings application.
+        /// </summary>
+        /// <returns>The view for the main ratings application.</returns>
+        [HttpGet]
+        [Route("Ratings")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
