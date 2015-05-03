9351603af2f0140eba04320813ebb77028530a7b
Matt Eland <Matt.Eland@Outlook.com>
2015-05-02 22:24:25 -0400

    Refactored map javascript to use namespacing / module patterns to prevent collisions.

--- AniWebApp/Scripts/maps.js
+++ AniWebApp/Scripts/maps.js
@@ -1,155 +1,165 @@
+// Define or use the namespace
+var MattEland = MattEland || {};
 
-var infobox = null;
+// Define the map module
+MattEland.Maps = (function () {
 
-function getIcon(typeId, severityId) {
+    var infobox = null;
 
-    /// <summary>
-    /// Gets the icon path for a map shape given the type and severity.
-    /// </summary>
-    /// <param name="typeId">The type identifier.</param>
-    /// <param name="severityId">The severity identifier.</param>
+    function getIcon(typeId, severityId) {
 
-    // Determine Shape Name based on type of item
-    var shapeName;
-    if (typeId === 1 || typeId === 3 || typeId === 8) { // Accidents
-        shapeName = "BowTie";
-    } else if (typeId === 5) {  // For misc. shapes
-        shapeName = "HalfDiamond";
-    } else { // All other shapes - primarily construction
-        shapeName = "Diamond";
+        /// <summary>
+        /// Gets the icon path for a map shape given the type and severity.
+        /// </summary>
+        /// <param name="typeId">The type identifier.</param>
+        /// <param name="severityId">The severity identifier.</param>
+
+        // Determine Shape Name based on type of item
+        var shapeName;
+        if (typeId === 1 || typeId === 3 || typeId === 8) { // Accidents
+            shapeName = "BowTie";
+        } else if (typeId === 5) { // For misc. shapes
+            shapeName = "HalfDiamond";
+        } else { // All other shapes - primarily construction
+            shapeName = "Diamond";
+        }
+
+        // Determine Color based on severity
+        var color;
+        if (severityId <= 1) {
+            color = "Blue";
+        } else if (severityId >= 3) {
+            color = "Red";
+        } else {
+            color = "Yellow";
+        }
+
+        // Put it all together in an icon
+        return "/Images/" + color + shapeName + ".png";
+
     }
 
-    // Determine Color based on severity
-    var color;
-    if (severityId <= 1) {
-        color = "Blue";
-    } else if (severityId >= 3) {
-        color = "Red";
-    } else {
-        color = "Yellow";
+    function hideInfobox(e) {
+
+        /// <summary>
+        /// Hides the info box.
+        /// </summary>
+        /// <param name="e">The event arguments.</param>
+
+        infobox.setOptions({ visible: false });
     }
 
-    // Put it all together in an icon
-    return "/Images/" + color + shapeName + ".png";
+    function displayInfobox(e) {
 
-}
+        /// <summary>
+        /// Displays the info box for the selected pushpin.
+        /// </summary>
+        /// <param name="e">The event arguments.</param>
 
-function addPin(map, lat, lng, severityId, severityName, title, description, start, end, typeId) {
-
-    /// <summary>
-    /// Builds and adds a pin to the map.
-    /// </summary>
-    /// <param name="map">The map.</param>
-    /// <param name="lat">The latitude.</param>
-    /// <param name="lng">The longitude.</param>
-    /// <param name="severityId">The severity identifier.</param>
-    /// <param name="severityName">Name of the severity.</param>
-    /// <param name="title">The title.</param>
-    /// <param name="description">The description.</param>
-    /// <param name="start">The start date/time.</param>
-    /// <param name="end">The end date/time.</param>
-    /// <param name="typeId">The type identifier.</param>
-
-    // Build the location of the pin
-    var location = new Microsoft.Maps.Location(lat, lng);
-
-
-    // Create a pin object
-    var pin = new Microsoft.Maps.Pushpin(location,
-    {
-        text: "",
-        icon: getIcon(typeId, severityId),
-        width: 16,
-        height: 16,
-        draggable: false
-    });
-
-    pin.Title = title;
-    pin.Description = description;
-    pin.SeverityName = severityName;
-    pin.Start = start;
-    pin.End = end;
-
-    // Add handler for the pushpin click event.
-    Microsoft.Maps.Events.addHandler(pin, 'click', displayInfobox);
-
-    // Add the items to the map
-    map.entities.push(pin);
-}
-
-function initializeMap(mapKey, lat, lng, zoom) {
-
-    /// <summary>
-    /// Initializes the map control.
-    /// </summary>
-    /// <param name="mapKey">The Bing map key.</param>
-    /// <param name="lat">The latitude.</param>
-    /// <param name="lng">The longitude.</param>
-    /// <param name="zoom">The zoom level.</param>
-
-    // Set up mapping options
-    var mapOptions = {
-        credentials: mapKey,
-        center: new Microsoft.Maps.Location(lat, lng),
-        mapTypeId: Microsoft.Maps.MapTypeId.road,
-        zoom: zoom,
-        showScalebar: false
+        if (e.targetType === 'pushpin') {
+            infobox.setLocation(e.target.getLocation());
+            infobox.setOptions({
+                visible: true,
+                title: e.target.SeverityName + ": " + e.target.Title,
+                description: e.target.Description + " " + e.target.Start + " - " + e.target.End
+            });
+        } else {
+            hideInfobox(e);
+        }
     }
 
-    // Create the map control
-    var map = new Microsoft.Maps.Map(document.getElementById("mapDiv"), mapOptions);
+    function initializeInfoBox(map) {
 
-    initializeInfoBox(map);
+        /// <summary>
+        /// Initializes the info box.
+        /// </summary>
+        /// <param name="map">The map.</param>
 
-    // Hide the infobox when the map is moved.
-    Microsoft.Maps.Events.addHandler(map, 'viewchange', hideInfobox);
-
-    return map;
-}
-
-
-function displayInfobox(e) {
-
-    /// <summary>
-    /// Displays the info box for the selected pushpin.
-    /// </summary>
-    /// <param name="e">The event arguments.</param>
-
-    if (e.targetType == 'pushpin') {
-        infobox.setLocation(e.target.getLocation());
-        infobox.setOptions({
-            visible: true, title: e.target.SeverityName + ": " + e.target.Title,
-            description: e.target.Description + " " + e.target.Start + " - " + e.target.End
+        // Create the infobox for the pushpins
+        infobox = new Microsoft.Maps.Infobox(new Microsoft.Maps.Location(0, 0),
+        {
+            visible: false,
+            offset: new Microsoft.Maps.Point(0, 0)
         });
-    } else {
-        hideInfobox(e);
+
+        // Add the box to the map
+        map.entities.push(infobox);
     }
-}
 
-function hideInfobox(e) {
+    return {
+        addPin: function(map, lat, lng, severityId, severityName, title, description, start, end, typeId) {
 
-    /// <summary>
-    /// Hides the info box.
-    /// </summary>
-    /// <param name="e">The event arguments.</param>
+            /// <summary>
+            /// Builds and adds a pin to the map.
+            /// </summary>
+            /// <param name="map">The map.</param>
+            /// <param name="lat">The latitude.</param>
+            /// <param name="lng">The longitude.</param>
+            /// <param name="severityId">The severity identifier.</param>
+            /// <param name="severityName">Name of the severity.</param>
+            /// <param name="title">The title.</param>
+            /// <param name="description">The description.</param>
+            /// <param name="start">The start date/time.</param>
+            /// <param name="end">The end date/time.</param>
+            /// <param name="typeId">The type identifier.</param>
 
-    infobox.setOptions({ visible: false });
-}
+            // Build the location of the pin
+            var location = new Microsoft.Maps.Location(lat, lng);
 
-function initializeInfoBox(map) {
 
-    /// <summary>
-    /// Initializes the info box.
-    /// </summary>
-    /// <param name="map">The map.</param>
+            // Create a pin object
+            var pin = new Microsoft.Maps.Pushpin(location,
+            {
+                text: "",
+                icon: getIcon(typeId, severityId),
+                width: 16,
+                height: 16,
+                draggable: false
+            });
 
-    // Create the infobox for the pushpins
-    infobox = new Microsoft.Maps.Infobox(new Microsoft.Maps.Location(0, 0),
-    {
-        visible: false,
-        offset: new Microsoft.Maps.Point(0, 0)
-    });
+            pin.Title = title;
+            pin.Description = description;
+            pin.SeverityName = severityName;
+            pin.Start = start;
+            pin.End = end;
 
-    // Add the box to the map
-    map.entities.push(infobox);
-}
+            // Add handler for the pushpin click event.
+            Microsoft.Maps.Events.addHandler(pin, 'click', displayInfobox);
+
+            // Add the items to the map
+            map.entities.push(pin);
+        },
+
+        initializeMap: function(mapKey, lat, lng, zoom) {
+
+            /// <summary>
+            /// Initializes the map control.
+            /// </summary>
+            /// <param name="mapKey">The Bing map key.</param>
+            /// <param name="lat">The latitude.</param>
+            /// <param name="lng">The longitude.</param>
+            /// <param name="zoom">The zoom level.</param>
+
+            // Set up mapping options
+            var mapOptions = {
+                credentials: mapKey,
+                center: new Microsoft.Maps.Location(lat, lng),
+                mapTypeId: Microsoft.Maps.MapTypeId.road,
+                zoom: zoom,
+                showScalebar: false
+            }
+
+            // Create the map control
+            var map = new Microsoft.Maps.Map(document.getElementById("mapDiv"), mapOptions);
+
+            initializeInfoBox(map);
+
+            // Hide the infobox when the map is moved.
+            Microsoft.Maps.Events.addHandler(map, 'viewchange', hideInfobox);
+
+            return map;
+        }
+    };
+
+}());
+
