1ffc65c4d37ee992b9e003e61778f65747d92a9f
Matt Eland <Matt.Eland@Outlook.com>
2015-05-05 23:01:38 -0400

    Using gulp for minification of JS.

--- AniWebApp/GulpFile.js
+++ AniWebApp/GulpFile.js
@@ -1,6 +1,11 @@
-﻿var gulp = require('gulp');
+﻿/// <binding BeforeBuild='minify' Clean='clean' />
+var gulp = require('gulp');
 var bower = require('gulp-bower');
 var del = require('del');
+var minifyCss = require("gulp-minify-css");
+var rename = require('gulp-rename');
+var uglify = require("gulp-uglify");
+
 var lib = '/lib';
 
 gulp.task('default', ['bower:install'], function () {
@@ -15,4 +20,31 @@ gulp.task('bower:install', ['clean'], function () {
 
 gulp.task('clean', function (done) {
     del(lib, done);
+});
+
+gulp.task('minify-css', ['clean'], function () {
+
+    gulp.src('./Content/Site.css')
+        .pipe(minifyCss())
+        .pipe(rename({ suffix: ".min", extname: ".css" }))
+        .pipe(gulp.dest("./Content"));
+
+});
+
+gulp.task('minify-js', ['clean'], function () {
+
+    gulp.src('./Scripts/maps.js')
+        .pipe(uglify())
+        .pipe(rename({ suffix: ".min", extname: ".js" }))
+        .pipe(gulp.dest("./Scripts"));
+
+    gulp.src('./Scripts/bootstrap-386.js')
+        .pipe(uglify())
+        .pipe(rename({ suffix: ".min", extname: ".js" }))
+        .pipe(gulp.dest("./Scripts"));
+
+});
+
+gulp.task('minify', ['minify-css', 'minify-js'], function () {
+
 });
