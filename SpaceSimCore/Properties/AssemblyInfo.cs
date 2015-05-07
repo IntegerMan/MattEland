9123956c26214538caa33fe12ee9c57921f14527
Matt Eland <Matt.Eland@Outlook.com>
2015-05-06 22:58:16 -0400

    Added Space Sim Core library for reference.

---
+++ SpaceSimCore/Properties/AssemblyInfo.cs
@@ -0,0 +1,39 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Space Sim Core")]
+[assembly: AssemblyDescription("Core Libraries for a Space Sim Component")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Matt Eland")]
+[assembly: AssemblyProduct("Space Sim Core Libraries")]
+[assembly: AssemblyCopyright("Copyright © Matt Eland 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e91449eb-2c6b-42a2-9093-9b0f935ea9d7")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en-US")]
+
