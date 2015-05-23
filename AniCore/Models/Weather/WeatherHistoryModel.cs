16edd1427a81eac9933bda72b2a558723c5f9f30
Matt Eland <matt.eland@gmail.com>
2015-05-22 22:06:56 -0400

    docs

--- AniCore/Models/Weather/WeatherHistoryModel.cs
+++ AniCore/Models/Weather/WeatherHistoryModel.cs
@@ -8,13 +8,24 @@ namespace Ani.Core.Models.Weather
 {
     public class WeatherHistoryModel
     {
-
+        /// <summary>
+        /// The zip code
+        /// </summary>
         public int ZipCode { get; set; }
 
+        /// <summary>
+        /// A list of weather predictions
+        /// </summary>
         public IList<WeatherPrediction> Predictions { get; set; } = new List<WeatherPrediction>();
 
-
+        /// <summary>
+        /// Forecast chart data for low temperatures
+        /// </summary>
         public string ForecastLowLineData;
+
+        /// <summary>
+        /// Forecast chart data for high temperatures
+        /// </summary>
         public string ForecastHighLineData;
     }
 }
