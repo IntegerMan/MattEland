85106234c5058bb4d454b178506530ff189b4e43
Matt Eland <Matt.Eland@gmail.com>
2015-10-10 17:04:59 -0400

    Added a basic search results shell.

---
+++ AniCore/Models/SearchModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Ani.Core.Models
+{
+    /// <summary>
+    ///     A data model for searching.
+    /// </summary>
+    public sealed class SearchModel
+    {
+        /// <summary>
+        ///     Gets or sets the search string.
+        /// </summary>
+        /// <value>
+        ///     The search string.
+        /// </value>
+        [DisplayName("Search Query")]
+        public string Query { get; set; }
+    }
+}
---
+++ AniWebApp/Controllers/SearchController.cs
@@ -0,0 +1,56 @@
+﻿using System.Web.Mvc;
+
+using Ani.Core.Models;
+
+using MattEland.Common;
+using MattEland.Common.Annotations;
+
+namespace AniWebApp.Controllers
+{
+    /// <summary>
+    ///     An MVC Controller controlling the application root.
+    /// </summary>
+    [Route(@"Search")]
+    public sealed class SearchController : CustomController
+    {
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="SearchController"/> class.
+        /// </summary>
+        public SearchController() : this(null)
+        {
+        }
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="CustomController" /> class.
+        /// </summary>
+        /// <param name="roleManager"> The role manager. </param>
+        public SearchController([CanBeNull] ApplicationRoleManager roleManager) : base(roleManager)
+        {
+        }
+
+        /// <summary>
+        ///     Serves up the search page's view.
+        /// </summary>
+        /// <returns>
+        ///     The search page view.
+        /// </returns>
+        [HttpGet]
+        [AllowAnonymous]
+        [Route(@"Search")]
+        public ActionResult Search([CanBeNull] SearchModel search)
+        {
+            // Ensure search exists
+            search = search ?? new SearchModel();
+
+            // If there was a query, run it
+            if (search.Query.HasText())
+            {
+                ShowSuccess("0 Results Found");
+            }
+
+            // Serve up the view
+            return View(search);
+        }
+
+    }
+}
