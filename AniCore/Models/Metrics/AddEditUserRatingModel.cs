f129bec147faac45ea4b79d7a7b59c45b5db4b47
Matt Eland <Matt.Eland@gmail.com>
2015-10-09 23:33:53 -0400

    Fixed tabs and optimized usings.

--- AniCore/Models/Metrics/AddEditUserRatingModel.cs
+++ AniCore/Models/Metrics/AddEditUserRatingModel.cs
@@ -4,34 +4,34 @@ using Ani.Core.Models.Users;
 
 namespace Ani.Core.Models.Metrics
 {
-    /// <summary>
-    /// A model used for adding or editing a user rating
-    /// </summary>
-    public class AddEditUserRatingModel : RatingModelBase
+	/// <summary>
+	/// A model used for adding or editing a user rating
+	/// </summary>
+	public class AddEditUserRatingModel : RatingModelBase
 	{
 
-        /// <summary>
-        /// Gets or sets the created time in UTC.
-        /// </summary>
-        /// <value>The created time in UTC.</value>
-        [Required]
+		/// <summary>
+		/// Gets or sets the created time in UTC.
+		/// </summary>
+		/// <value>The created time in UTC.</value>
+		[Required]
 		[DisplayName("Created Date")]
 		[DataType(DataType.DateTime)]
 		public System.DateTime CreatedTimeUTC { get; set; }
 
-        /// <summary>
-        /// Gets or sets the modified time in UTC format.
-        /// </summary>
-        /// <value>The modified time in UTC.</value>
-        [Required]
+		/// <summary>
+		/// Gets or sets the modified time in UTC format.
+		/// </summary>
+		/// <value>The modified time in UTC.</value>
+		[Required]
 		[DisplayName("Modified Date")]
 		[DataType(DataType.DateTime)]
 		public System.DateTime ModifiedTimeUTC { get; set; }
 
-        /// <summary>
-        /// Gets or sets the user.
-        /// </summary>
-        /// <value>The user.</value>
-        public UserModel User { get; set; }
+		/// <summary>
+		/// Gets or sets the user.
+		/// </summary>
+		/// <value>The user.</value>
+		public UserModel User { get; set; }
 	}
 }
--- AniCore/Models/Metrics/RatingModelBase.cs
+++ AniCore/Models/Metrics/RatingModelBase.cs
@@ -4,58 +4,58 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Ani.Core.Models.Metrics
 {
-    public class RatingModelBase //: IValidatableObject
-    {
-        /// <summary>
-        /// Gets or sets the rating value.
-        /// </summary>
-        /// <value>The rating value.</value>
-        [Required]
-        [DisplayName("Rating")]
-        public int RatingValue { get; set; }
+	public class RatingModelBase //: IValidatableObject
+	{
+		/// <summary>
+		/// Gets or sets the rating value.
+		/// </summary>
+		/// <value>The rating value.</value>
+		[Required]
+		[DisplayName("Rating")]
+		public int RatingValue { get; set; }
 
-        /// <summary>
-        /// Gets or sets the rating model.
-        /// </summary>
-        /// <value>The rating model.</value>
-        public RatingModel Rating { get; set; }
+		/// <summary>
+		/// Gets or sets the rating model.
+		/// </summary>
+		/// <value>The rating model.</value>
+		public RatingModel Rating { get; set; }
 
-        /// <summary>
-        /// Gets or sets the entry date.
-        /// </summary>
-        /// <value>The entry date.</value>
-        [Required]
-        [DisplayName("Date")]
-        [DataType(DataType.Date)]
-        public System.DateTime EntryDate { get; set; }
+		/// <summary>
+		/// Gets or sets the entry date.
+		/// </summary>
+		/// <value>The entry date.</value>
+		[Required]
+		[DisplayName("Date")]
+		[DataType(DataType.Date)]
+		public System.DateTime EntryDate { get; set; }
 
-        /// <summary>
-        /// Gets or sets the comments.
-        /// </summary>
-        /// <value>The comments.</value>
-        [DisplayName("Comments")]
+		/// <summary>
+		/// Gets or sets the comments.
+		/// </summary>
+		/// <value>The comments.</value>
+		[DisplayName("Comments")]
 		[DataType(DataType.MultilineText)]
 		public string Comments { get; set; }
 
-        /// <summary>
-        /// Validates the model.
-        /// </summary>
-        /// <param name="validationContext">The validation context.</param>
-        /// <returns>A yielded collection of validation results.</returns>
-        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
-        {
-            if (this.RatingValue < this.Rating.MinValue)
-            {
-                var message = string.Format("Rating Value cannot be less than the minimum value ({0})", this.Rating.MinValue);
-                yield return new ValidationResult(message, new List<string> {"RatingValue"});
-            }
+		/// <summary>
+		/// Validates the model.
+		/// </summary>
+		/// <param name="validationContext">The validation context.</param>
+		/// <returns>A yielded collection of validation results.</returns>
+		public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+		{
+			if (this.RatingValue < this.Rating.MinValue)
+			{
+				var message = string.Format("Rating Value cannot be less than the minimum value ({0})", this.Rating.MinValue);
+				yield return new ValidationResult(message, new List<string> {"RatingValue"});
+			}
 
-            if (this.RatingValue > this.Rating.MaxValue)
-            {
-                var message = string.Format("Rating Value cannot be greater than the maximum value ({0})", this.Rating.MaxValue);
-                yield return new ValidationResult(message, new List<string> {"RatingValue"});
-            }
+			if (this.RatingValue > this.Rating.MaxValue)
+			{
+				var message = string.Format("Rating Value cannot be greater than the maximum value ({0})", this.Rating.MaxValue);
+				yield return new ValidationResult(message, new List<string> {"RatingValue"});
+			}
 
-        }
-    }
+		}
+	}
 }
